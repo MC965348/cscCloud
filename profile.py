bec1c8fcb3e9c996bcd2201e02c0a415c8a51d7d
Michael Collins <98608611+MC965348@users.noreply.github.com>
2022-10-27 15:46:10 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,32 @@
-#make v machine named node
-#vm has public ip address
-
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
+import geni.rspec.igext as IG
 
-request = portal.context.makeRequestRSpec()
+pc = portal.Context()
+request = pc.makeRequestRSpec()
 
-node = request.XenVM("node")
+tourDescription = \
+"""
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+"""
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+node = request.XenVM("docker")
+node.cores = 8
+node.ram = 8192
+node.routable_control_ip = "true" 
+
+bs_landing = node.Blockstore("bs_image", "/image")
+bs_landing.size = "500GB"
+  
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
-
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo bash /local/repository/install_docker.sh"))
-
-
-portal.context.printRequestRSpec()
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+  
+pc.printRequestRSpec(request)
